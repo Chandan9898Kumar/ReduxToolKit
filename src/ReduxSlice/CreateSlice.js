5c245ab01aeb6ac9df6709c7193535c915ff661b
Chandan <Chandan17ee@gmail.com>
2023-08-26 00:49:17 +0530

    chore:added new comments

--- src/ReduxSlice/CreateSlice.js
+++ src/ReduxSlice/CreateSlice.js
@@ -16,6 +16,13 @@ export const initialState = {
 
 //  Here we don't need to pass dispatch function with type,createSlice itself create type.
 //  we just need to create a function and export it. And import these functions in your files.
+
+
+// NOTE   -  
+
+// createSlice function looks at all the functions that are defined in the reducers field and 
+// for every case generates an action creator that uses the name of the reducer as the action type itself.
+
 const sessionSlice = createSlice({
   name: "sessionRecord",
   initialState,
@@ -97,3 +104,16 @@ export const {
 
 //  Import below in Store file.
 export default sessionSlice.reducer;
+
+
+
+// What is createSlice in Redux Toolkit  ?
+
+// createSlice is a higher order function that accepts an initial state, 
+// an object full of reducer functions and a slice name. 
+// It automatically generates action creators and action types that correspond to the reducers and state.
+
+// In Redux-Toolkit, the createSlice method helps us create a slice of the redux-store. 
+// This function aims to reduce the boilerplate required to add data to redux in the canonical way. 
+// Internally, it uses createAction and createReducer.
+
--- src/Store.js
+++ src/Store.js
@@ -6,3 +6,24 @@ export default configureStore({
     ValidationData: sessionSlice,
   },
 });
+
+
+
+
+// NOTE         -
+
+// configureStore is only accepting one parameter, which is an Object, which is called ConfigureStoreOptions.
+
+// ConfigureStoreOptions has several attributes (? means optional):
+
+// reducers
+// middleware​?
+// devTools​?
+// preloadedState​?
+// enhancers​?
+
+// The most important and easy to understand are reducers, devTools, and preloadedState.
+
+// reducers (object)
+// the reducers attribute is the mandatory option that we put our reducers as attributes.
+// The attribute name will be related to selector.
